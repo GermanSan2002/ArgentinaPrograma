a49240ad2d0f295806c0be35f432b34bea084f1b
GermanSan2002 <germansan2002@outlook.com>
2023-01-07 22:28:08 -0300

    Cambio de color js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,13 @@
-const toggleTheme = document.getElementById('toggle-theme');
-const toggleIcon = document.getElementById('toggle-icon');
-const toggleText = document.getElementById('toggle-text');
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-icon");
+const toggleText = document.getElementById("toggle-text");
 
-toggleTheme.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    if (toggleIcon.src.includes('moon.svg')) {
+const toggleColor = document.getElementById("toogle-colors");
+const rootStyles = document.documentElement.style
+
+toggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (toggleIcon.src.includes("moon.svg")) {
         toggleIcon.src = "ASSETS/ICON/sun.svg";
         toggleText.textContent = "Light mode";
     }
@@ -12,4 +15,8 @@ toggleTheme.addEventListener('click', () => {
         toggleIcon.src = "ASSETS/ICON/moon.svg";
         toggleText.textContent = "Dark mode";
     }
+});
+
+toggleColor.addEventListener("click", (e) => {
+    rootStyles.setProperty('--primary-color', e.target.dataset.color);
 });
